05a348a957709866f736df85077d500612fa4733
legiang1109 <legiang1109@gmail.com>
2023-06-07 16:09:46 +0700

    bài#32

--- src/seeders/seeder-users.js
+++ src/seeders/seeder-users.js
@@ -4,9 +4,13 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert("Users", [
       {
-        firstName: "John",
-        lastName: "Doe",
-        email: "example@example.com",
+        email: "admin@gmail.com",
+        password: "123456",
+        firstName: "Giang",
+        lastName: "Le",
+        address: "VN",
+        gender: 1,
+        roleId: "1",
         createdAt: new Date(),
         updatedAt: new Date(),
       },
