edff1c37f8720fc3bed75fdafe9771dc14ae89d5
legiang1109 <legiang1109@gmail.com>
2023-06-09 17:10:56 +0700

    bài#34

--- src/controllers/homeController.js
+++ src/controllers/homeController.js
@@ -1,4 +1,5 @@
 import db from "../models/index";
+import CRUDService from "../services/CRUDService";
 
 let getHomePage = async (req, res) => {
   try {
@@ -17,6 +18,15 @@ let getHomePage = async (req, res) => {
 let getAboutPage = (req, res) => {
   return res.render("test/about.ejs");
 };
+let getCRUD = (req, res) => {
+  return res.render("crud.ejs");
+};
+
+let postCRUD = async (req, res) => {
+  let message = await CRUDService.createNewUser(req.body);
+  console.log(message);
+  return res.send("post crud from sever");
+};
 
 // object: {
 //     key: '',
@@ -25,4 +35,6 @@ let getAboutPage = (req, res) => {
 module.exports = {
   getHomePage: getHomePage,
   getAboutPage: getAboutPage,
+  getCRUD: getCRUD,
+  postCRUD: postCRUD,
 };
